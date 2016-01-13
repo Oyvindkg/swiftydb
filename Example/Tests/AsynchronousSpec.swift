6a600a9b51b03498cc9cec5f79d5f207a021f72d
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-01-13 22:51:50 +0100

    Improved async testing

--- Example/Tests/AsynchronousSpec.swift
+++ Example/Tests/AsynchronousSpec.swift
@@ -18,22 +18,86 @@ class AsynchronousSpec: SwiftyDBSpec {
         let database = SwiftyDB(databaseName: "test_database")
         
         
-        var success = false
         
-        waitUntil { done in
-            database.addObjects([TestClass()]) { (result) -> Void in
-                switch result {
-                case .Success:
-                    success = true
-                case .Error:
-                    success = false
+        describe("Asynchronous calls") {
+            context("Adding data to the database") {
+                var success = false
+                
+                waitUntil { done in
+                    database.addObjects([TestClass()]) { (result) -> Void in
+                        switch result {
+                        case .Success:
+                            success = true
+                        case .Error:
+                            success = false
+                        }
+                        done()
+                    }
                 }
-                done()
+                
+                it("Should add objects") {
+                    expect(success).to(beTrue())
+                }
+                
+                success = false
+                waitUntil { done in
+                    database.addObject(TestClass()) { (result) -> Void in
+                        switch result {
+                        case .Success:
+                            success = true
+                        case .Error:
+                            success = false
+                        }
+                        done()
+                    }
+                }
+                
+                it("Should add object") {
+                    expect(success).to(beTrue())
+                }
             }
-        }
-        
-        it("should add data") {
-            expect(success).to(beTrue())
+            
+            context("Should retrieve data") {
+                let object = TestClass()
+                try? database.addObject(object)
+                
+                var retrievedData: Any?
+                waitUntil { done in
+                    database.dataForType(TestClass.self, matchingFilters: ["primaryKey": object.primaryKey], withCompletionHandler: { (result) -> Void in
+                        switch result {
+                        case .Success(let data):
+                            retrievedData = data
+                        default:
+                            break
+                        }
+                        done()
+                    })
+                }
+                
+                it("Should retrieve object") {
+                    expect(retrievedData).notTo(beNil())
+                }
+            }
+            
+            context("Should delete object") {
+                let object = TestClass()
+                try? database.addObject(object)
+                
+                var success = false
+                waitUntil { done in
+                    database.deleteObjectsForType(TestClass.self, matchingFilters: ["primaryKey": object.primaryKey], withCompletionHandler: { (result) -> Void in
+                        switch result {
+                        case .Success:
+                            success = true
+                        case .Error:
+                            success = false
+                        }
+                        done()
+                    })
+                }
+                
+                expect(success).to(beTrue())
+            }
         }
     }
 }
