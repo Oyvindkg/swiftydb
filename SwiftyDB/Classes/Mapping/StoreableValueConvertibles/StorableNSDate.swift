9ceda6b72f9652de940577e0df1cfd32d7bc82ea
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-09-22 21:57:27 +0200

    Added new StorableNSDate

---
+++ SwiftyDB/Classes/Mapping/StoreableValueConvertibles/StorableNSDate.swift
@@ -0,0 +1,35 @@
+//
+//  NSDate.swift
+//  SwiftyDB
+//
+//  Created by Øyvind Grimnes on 20/08/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    
+    return dateFormatter
+}()
+
+extension NSDate: StorableProperty {
+    
+    public typealias StorableValueType = String
+    
+    public var storableValue: StorableValueType {
+        return dateFormatter.string(from: self as Date)
+    }
+    
+    public static func fromStorableValue(_ storableValue: StorableValueType) -> Self {
+        return fromHelper(storableValue: storableValue)
+    }
+    
+    private static func fromHelper<T>(storableValue: StorableValueType) -> T {
+        return dateFormatter.date(from: storableValue) as! T
+    }
+}
