05feb1d4c937221370438128b7e912398cadeedc
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-09-22 14:58:21 +0200

    Target iOS 8.0

---
+++ SwiftyDB/SwiftyDB.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDB.h
+//  SwiftyDB
+//
+//  Created by Øyvind Grimnes on 22/09/16.
+//  Copyright © 2016 Øyvind Grimnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDB.
+FOUNDATION_EXPORT double SwiftyDBVersionNumber;
+
+//! Project version string for SwiftyDB.
+FOUNDATION_EXPORT const unsigned char SwiftyDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDB/PublicHeader.h>
+
+
