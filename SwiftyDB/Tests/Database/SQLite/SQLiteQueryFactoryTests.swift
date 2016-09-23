cb7a125c02e0343780d7c7c086dc716b37bded7d
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-09-23 19:52:35 +0200

    Updated for swift 3.0

--- SwiftyDB/Tests/Database/SQLite/SQLiteQueryFactoryTests.swift
+++ SwiftyDB/Tests/Database/SQLite/SQLiteQueryFactoryTests.swift
@@ -15,12 +15,12 @@ import XCTest
 class SQLiteQueryFactoryTests: XCTestCase {
     
     var sut = SQLiteQueryFactory()
-    var reader = Mapper.readerForType(Stark.self)
+    var reader = Mapper.readerFor(type: Stark.self)
     
     override func setUp() {
         super.setUp()
         
-        reader = Mapper.readerForType(Stark.self)
+        reader = Mapper.readerFor(type: Stark.self)
         sut = SQLiteQueryFactory()
     }
     
@@ -88,7 +88,7 @@ class SQLiteQueryFactoryTests: XCTestCase {
         let lady  = Wolf(name: "Lady", age: 3)
         let sansa = Stark(name: "Sansa", weight: 50, age: 14, wolf: lady)
         
-        let reader = Mapper.readerForObject(sansa)
+        let reader = Mapper.readerFor(object: sansa)
         
         let query = sut.insertQueryForReader(reader)
         
@@ -102,7 +102,7 @@ class SQLiteQueryFactoryTests: XCTestCase {
         let lady  = Wolf(name: "Lady", age: 3)
         let sansa = Stark(name: "Sansa", weight: 50, age: 14, wolf: lady)
         
-        let reader = Mapper.readerForObject(sansa)
+        let reader = Mapper.readerFor(object: sansa)
         
         let query = sut.insertQueryForReader(reader)
         
